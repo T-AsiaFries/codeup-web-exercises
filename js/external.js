fa757525a284c1abcf3621226e8072d51c95c756
T-Asia Fries <tasia.s.fries@gmail.com>
2023-10-12 16:58:12 -0500

    Finished number 2 of 2nd part of exercise

--- js/external.js
+++ js/external.js
@@ -1,1 +1,5 @@
 console.log("Hello from external JavaScript")
+alert("Welcome to my website!")
+
+let userInput = prompt("What is your favorite color")
+console.log(`My favorite color is ${userInput} too`)
