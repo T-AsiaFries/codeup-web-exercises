781ec9b885caa1289c40c09f1cb101fbfadcaf27
T-Asia Fries <tasia.s.fries@gmail.com>
2023-10-12 09:10:03 -0500

    warm-up exercise

---
+++ warm-ups/warmUps.js
@@ -0,0 +1,7 @@
+//T-Asia Fries
+//Daily Warm-Ups
+let name = "T-Asia Fries"
+console.log(name)
+let year = "2015"
+console.log(year)
+console.log(`My name is ${name} and I graduated highschool in ${year}`)
