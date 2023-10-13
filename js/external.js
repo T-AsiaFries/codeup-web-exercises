9dede7ba7585c86807c2dec4db8f12905710fdf3
T-Asia Fries <tasia.s.fries@gmail.com>
2023-10-12 20:54:21 -0500

    Finished exercise

--- js/external.js
+++ js/external.js
@@ -3,3 +3,35 @@ alert("Welcome to my website!")
 
 let userInput = prompt("What is your favorite color")
 console.log(`My favorite color is ${userInput} too`)
+
+let littleMermaid= 3;
+let brotherBear = 5;
+let hercules = 1;
+let totalPrice = (littleMermaid + brotherBear + hercules) * 3
+console.log(totalPrice)
+
+userInput = prompt("What is (3 + 5 + 1) * 3")
+console.log(`(3 + 5 + 1) * 3 is ${27}`)
+alert("27")
+
+let google = 400 * 6;
+let amazon = 380 * 4;
+let facebook = 350 * 10;
+let overallPrice = (google + amazon + facebook)
+console.log(overallPrice)
+
+userInput = prompt("What is 400 * 6")
+console.log(`400 * 6 is ${2400}`)
+alert("2400")
+
+userInput = prompt("What is 300 * 4")
+console.log(`300 * 6 is ${1520}`)
+alert("1520")
+
+userInput = prompt("What is 350 * 10")
+console.log(`350 * 10 is ${3500}`)
+alert("3500")
+
+userInput = prompt("What is 2400 + 1520 + 3500")
+console.log(`2400 + 1520 + 3500 is ${7420}`)
+alert("7420")
