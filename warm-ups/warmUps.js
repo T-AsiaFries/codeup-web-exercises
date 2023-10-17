8d19b219910257566234afa6163109b47d9a5ab4
T-Asia Fries <tasia.s.fries@gmail.com>
2023-10-17 09:48:09 -0500

    Finished warmup

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -3,3 +3,22 @@
 let name = "T-Asia Fries"
 let year = "2015"
 console.log(`My name is ${name} and I graduated highschool in ${year}`)
+
+//Daily warm-up
+//10.17
+//Create a function called largestNum, where they take in 3 numbers as input and send back the largest out of the three numbers.
+
+let num1 = 2
+let num2 = 4
+let num3 = 6
+
+console.log(Math.max(num1,num2,num3));
+
+////
+
+function largestNum (num1,num2,num3) {
+    return Math.max(num1,num2,num3) + " " + 'is the greatest';
+}
+
+console.log(largestNum(num1,num2,num3));
+
