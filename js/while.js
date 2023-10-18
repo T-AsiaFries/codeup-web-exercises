15ca8fee76c31ed20d53525fb4a02c4a6550d356
T-Asia Fries <tasia.s.fries@gmail.com>
2023-10-18 15:36:07 -0500

    Finished while/do-while loop exercise

---
+++ js/while.js
@@ -0,0 +1,25 @@
+// While loop
+
+let num = 1;
+
+while(num < 65536) {
+    console.log(num = num * 2);
+}
+
+// Do while loop
+
+let allCones = Math.floor(Math.random() * 50) + 50;
+
+// Cannot put allCones inside the body because it will keep generating a random number.
+
+do {
+    let conesSold = Math.floor(Math.random() * 5) + 1;
+    if(allCones < conesSold) {
+        console.log("Yay! I sold them all!")
+        break;
+    } else {
+        allCones = allCones - conesSold;
+        console.log(conesSold + " sold");
+        console.log(allCones + " all cones are left")
+    }
+} while (allCones > 0)
