8bacb869921a59ab84f8be3964272520560cf820
T-Asia Fries <tasia.s.fries@gmail.com>
2023-10-18 17:21:15 -0500

    added similar solutions to exercise

--- js/while.js
+++ js/while.js
@@ -6,6 +6,14 @@ while(num < 65536) {
     console.log(num = num * 2);
 }
 
+// Similar solution ^^
+
+// let num = 1;
+// while(num < 65536) {
+//     num *=2;
+//     console.log(num)
+// }
+
 // Do while loop
 
 let allCones = Math.floor(Math.random() * 50) + 50;
@@ -23,4 +31,17 @@ do {
         console.log(allCones + " all cones are left")
     }
 } while (allCones > 0)
+
+// Similar solution
+
+// do {
+//     let conesSold = Math.floor(Math.random() * 5) + 1
+//     if(conesSold <= allCones) {
+//         console.log(`${conesSold} cones sold...`)
+//         allCones -= conesSold
+//     } else {
+//         console.log(`Cannot sell you ${conesSold} cones. I only have ${conesSold}`)
+//     }
+// } while (allCones !== 0)
+// console.log("Yay! I sold them all")
 
