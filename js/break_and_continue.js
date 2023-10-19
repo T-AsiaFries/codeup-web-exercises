362c1b8b03fb08378c05c2bea9e911727f411e36
T-Asia Fries <tasia.s.fries@gmail.com>
2023-10-19 13:52:58 -0500

    finished break-and-continue exercise

---
+++ js/break_and_continue.js
@@ -0,0 +1,15 @@
+let userInput=prompt("Enter a odd number between 1 and 50")
+
+while (isNaN(userInput) || userInput % 2 === 0) {
+    userInput = parseInt(prompt("Please try again"));
+}
+
+for (let i = 1; i <50; i+= 2) {
+    console.log("Here is an odd number " + i)
+    if(i == userInput) {
+        console.log("Yikes! Skipping number " + i)
+        continue;
+    }
+}
+
+// Continue skips the code below
