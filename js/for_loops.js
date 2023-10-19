6d106fa1f716ee6f837222c0c37cf98b4e0c9864
T-Asia Fries <tasia.s.fries@gmail.com>
2023-10-19 11:54:28 -0500

    still working

---
+++ js/for_loops.js
@@ -0,0 +1,29 @@
+// 2.
+
+function showMultiplicationTable(num) {
+    for (let i = 1; i <=10; i++) {
+        console.log(num + " * " + i + " = " + num * i)
+    }
+}
+
+showMultiplicationTable(7);
+
+
+for (let i = 0 ; i < 10; i++) {
+    let random = Math. floor(Math. random() * ((200-20)+1) + 20);
+    if(random % 2 === 0) {
+        console.log(random + " is even")
+    } else if(random % 2 !== 0) {
+        console.log(random + " is odd")
+    }
+}
+
+// 4.
+
+
+
+// 5.
+
+// for (let i = 5; i < 100; i++) {
+//     console.log(i)
+// }
