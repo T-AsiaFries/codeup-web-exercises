dbc55ebe60da34091ebdbff9aa6adbb75ed8647b
T-Asia Fries <tasia.s.fries@gmail.com>
2023-10-19 15:04:22 -0500

    exercise walkthrough

--- js/for_loops.js
+++ js/for_loops.js
@@ -8,6 +8,7 @@ function showMultiplicationTable(num) {
 
 showMultiplicationTable(7);
 
+// 3.
 
 for (let i = 0 ; i < 10; i++) {
     let random = Math. floor(Math. random() * ((200-20)+1) + 20);
@@ -24,8 +25,22 @@ for (let i = 0; i <10; i++) {
     console.log(i.toString().repeat(i))
 }
 
+// Another solution
+
+// for (let i = 1; i < 10; i++) {
+//     let num = ""
+//     for (let j = 0; j < i; i++) {
+//         num = num + i;
+//     }
+//     console.log(num)
+// }
+
 // 5.
 
 for (let i = 100; i > 0; i-=5) {
     console.log(i)
 }
+
+// for (let i = 100; i >= 5; i-=5) {
+//     console.log(i)
+// }
