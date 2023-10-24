38bc1de32f6ad8ebaf5e7f88cf19fc426ecd6133
T-Asia Fries <tasia.s.fries@gmail.com>
2023-10-24 09:51:01 -0500

    finished circle exercise walkthrough

--- js/circle.js
+++ js/circle.js
@@ -5,19 +5,19 @@ var circle = {
     radius: 3,
 
     getArea: function () {
-        return Math.PI * Math.pow(this.radius, 2);
+        return Math.PI * Math.pow(circle.radius, 2);
     },
 
     logInfo: function (doRounding) {
         // TODO: complete this method.
         let area = this.getArea()
-        if (doRounding === true) {
-           area = Math.round(area)
+        if (doRounding) {
+            //if doRounding is true, round the result to the nearest integer
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
+        } else {
+            // Otherwise, output the complete value
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea());
         }
-        // If doRounding is true, round the result to the nearest integer.
-        // Otherwise, output the complete value
-
-        console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
     }
 };
 
@@ -30,7 +30,7 @@ circle.logInfo(true);
 console.log("=======================================================");
 // TODO: Change the radius of the circle to 5.
 
-
+circle.radius = 5;
 
 // log info about the circle
 console.log("Raw circle information");
