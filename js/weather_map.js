3acade68a91971264802cf9be52432b104c4082f
T-Asia Fries <tasia.s.fries@gmail.com>
2023-12-01 13:43:47 -0600

    still working weather map js

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,22 +1,50 @@
-// fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Irving&appid=${WEATHER_MAP_API}`)
-//     .then(response => response.json())
-//     .then(data => {
-//         const forecastContainer = document.getElementById('forecast')
-//         data.list.forEach(item => {
-//             const date = new Date(item.dt * 1000);
-//             const temperature = item.main.temp;
-//             const icon = item.weather[0].icon;
-//
-//
-//             const forecastCard = document.createElement('div');
-//             forecastCard.classList.add('col-md-2', 'mb-3');
-//             forecastCard.innerHTML = `
-//                     <p>${date.toDateString()}</p>
-//                     <p>${temperature} °C</p>
-//                     <img src="http://openweathermap.org/img/w/${icon}.png" alt="Weather Icon">
-//                    `;
-//         });
-//         forecastContainer.appendChild(forecastCard);
-//     })
-//     .catch(error => console.error('Error fetching forecast:', error));
-// })
+fetch(`http://api.openweathermap.org/data/2.5/forecast?` + `lat=32.813499&lon=-96.955963` + `&appid=${WEATHER_MAP_API}`)
+    .then(response => response.json())
+    .then(data => {
+        const forecastContainer = document.getElementById('forecast-container')
+        // data.list.forEach(item => {
+        console.log(data)
+        let day = data.list;
+        for(let i = 0; i < day.length; i += 8) {
+            let weather = day[i];
+
+            const date = new Date(weather.dt * 1000);
+            const formattedDate = formatDate(date);
+            const temperature = weather.main.temp;
+            const description = weather.weather[0].description;
+            const humidity = weather.main.humidity;
+            const wind = weather.wind.speed;
+            const pressure = weather.main.pressure;
+
+            const forecastCard = document.createElement('div');
+            forecastCard.classList.add('forecast-card','col-md-2');
+            forecastCard.innerHTML = `
+                <div class="card">
+                    <div class="card-header">
+                        <p class="top">${formattedDate}</p>
+                    </div>
+                     <div class="card-body">
+                        <div>
+                            <p class="top">${temperature} °F</p>
+                            <img src="http://openweathermap.org/img/w/${weather.weather[0].icon}.png" alt="Weather Icon" class="weather-icon">
+                        </div>
+                         <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Description: ${description}</li>
+                        <li class="list-group-item">Humidity: ${humidity}</li>
+                        <li class="list-group-item">Wind: ${wind}</li>
+                        <li class="list-group-item">Pressure: ${pressure}</li>
+                    </ul>
+                    </div>
+                </div>
+            `;
+            forecastContainer.appendChild(forecastCard);
+        }
+
+    })
+    .catch(error => console.error('Error:', error))
+
+function formatDate(date) {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return date.toLocaleDateString('en-US', options).split('/').reverse().join('-');
+}
+
